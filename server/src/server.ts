063b614ffa59fbe08777bcaae4c9ce65f8dc63f0
Italo Castro <italocesarcastroo@gmail.com>
2023-01-18 12:49:45 -0300

    criando as rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,13 @@
 import Fastify from 'fastify';
 import cors from "@fastify/cors";
-import { PrismaClient } from '@prisma/client';
-
+import { prisma } from './lib/prisma';
+import { appRoutes } from './route';
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors); // aqui por exemplo eu indico quais endereços podem acessar o back end
+app.register(appRoutes);
 
-/**
- * Http: Get, Post, Put, Patch, Delete
- */
-
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany({})
-    return habits;
-});
 
 app.listen({
     port: 3333
